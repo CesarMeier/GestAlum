ee202183ce31660413a8d0f2a36f297bc25649f4
CesarMeier <cesar44135797@gmail.com>
2025-06-17 20:18:52 -0300

    cambio de correo del git bash

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Index</title>
+    <title>CRUD Index CesarMeier</title>
 
     <link rel="stylesheet" href="public/bootstrap/css/bootstrap.min.css">
     <!-- Link de DataTable-->
